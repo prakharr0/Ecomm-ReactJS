e698882810e75bd6bd077cd1755cbdaa28a30140
Prakhar Rathi <prakhar@mail.uni-paderborn.de>
2020-12-18 14:02:31 +0100

    Header complete

--- ecommerce-app/src/Components/HeaderComponent.jsx
+++ ecommerce-app/src/Components/HeaderComponent.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './Header.css'
 import logo from './logo.png'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 
 class HeaderComponent extends Component {
     render() {
         return (
             <div className = "header">
-                <div className = "header__logo">
-                  <img className="photo" src={logo} />
-                </div>
+              
+                  <img className="header__logo" src={logo} />
 
                 <div className="header__search">
                     <input placeholder = "Search" className="header_search_input" type = "text"/>
+                    <SearchIcon className = "header__search_icon"></SearchIcon>
                 </div>
                 
                 <div className = "header__nav">
@@ -20,17 +22,33 @@ class HeaderComponent extends Component {
                         <span className = "header__option_line1">
                             Hello User
                         </span>
-                    </div>
-
-                    <div className = "header__option">
                         <span className = "header__option_line2">
-                            Orders/Return
+                            Sign In
                         </span>
                     </div>
 
                     <div className = "header__option">
-                        <span className = "header__option_line3">
-                            Prime
+                        <span className = "header__option_line1">
+                            View Your
+                        </span>
+                        <span className = "header__option_line2">
+                            Orders/Returns
+                        </span>
+                    </div>
+
+                    <div className = "header__option">
+                    <span className = "header__option_line1">
+                            Your
+                        </span>
+                        <span className = "header__option_line2">
+                            Settings
+                        </span>
+                    </div>
+
+                    <div className="header_basket_option">
+                        <ShoppingBasketIcon/>
+                        <span className = "header__option_line2 header_basket_count">
+                            0
                         </span>
                     </div>
                 </div>
